8a7a62f120c688ff47cddd7b10095bfa47449259
ultima57 <artyomzar83@gmail.com>
2022-10-18 11:06:39 +0300

    added Map.java

--- Map.java
+++ Map.java
@@ -2,5 +2,6 @@ public class Map {
     public static void main(String[] args) {
         System.out.println("ee");
         System.out.println("3232323");
+        System.out.println(" 444");
     }
 }
